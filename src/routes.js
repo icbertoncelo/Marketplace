d6f62ba47a90ece19f8c4c863c2b616c7c546ec4
Ian Carlos <carlos.ian007@gmail.com>
2019-04-30 23:52:35 -0300

    Feat(auth): Add Authorization and jwt

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,14 @@ const express = require('express')
 const routes = express.Router()
 
 const UserController = require('./app/controllers/UserController')
+const SessionController = require('./app/controllers/SessionController')
+const authMiddleware = require('./app/middlewares/auth')
 
 routes.post('/users', UserController.store)
+routes.post('/sessions', SessionController.store)
+
+routes.get('/test', authMiddleware, (req, res) => {
+  res.json({ ok: true })
+})
 
 module.exports = routes
